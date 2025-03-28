506fea20494bb13973f6f3d03bf223b4da442d45
cadafy <cadafy69@gmail.com>
2025-03-28 17:47:26 -0400

    Agrego ruta de Noticias

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Fotos from "./pages/Fotos";
+import Noticias from "./pages/Noticias";
+import Galeria from "./pages/Galería";
+import Reproducciones from "./pages/Reproducciones";
 import SubeTuMix from "./pages/SubeTuMix";
 import Politica from "./pages/Politica";
 import Contacto from "./pages/Contacto";
-import Home from "./pages/Home";
-import Galeria from "./pages/Galería";
-import Fotos from "./pages/Fotos";
-import Noticias from "./pages/Noticias";
-import Reproducciones from "./pages/Reproducciones";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/fotos" element={<Fotos />} />
+        <Route path="/noticias" element={<Noticias />} />
+        <Route path="/galeria" element={<Galeria />} />
+        <Route path="/reproducciones" element={<Reproducciones />} />
         <Route path="/sube-tu-mix" element={<SubeTuMix />} />
         <Route path="/politica" element={<Politica />} />
         <Route path="/contacto" element={<Contacto />} />
-        <Route path="/galeria" element={<Galeria />} />
-        <Route path="/fotos" element={<Fotos />} />
-        <Route path="/noticias" element={<Noticias />} />
-        <Route path="/reproducciones" element={<Reproducciones />} />
       </Routes>
     </Router>
   );
